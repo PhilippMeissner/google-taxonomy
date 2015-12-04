ede4cd012bcc1a1ce2097249f058f05d091e5313
Christian Semmler <mail@csemmler.com>
2015-12-04 13:42:59 +0100

    update taxonomies

--- lib/google/taxonomy/validator.rb
+++ lib/google/taxonomy/validator.rb
@@ -4,25 +4,25 @@ module Google
   module Taxonomy
     class Validator
 
-      def self.entries
-        @@entries ||= convert_entries
+      def self.entries(lang = 'de-DE')
+        @@entries ||= convert_entries(lang)
       end
 
-      def self.convert_entries
+      def self.convert_entries(lang = 'de-DE')
         entries = []
-        File.open(file_location, "r").each_with_index do |line, index|
+        File.open(file_location(lang), "r").each_with_index do |line, index|
           next if index == 0
           entries << line.gsub("\n", "")
         end
         entries.to_set
       end
 
-      def self.file_location
-        File.expand_path('../../data_files/taxonomy.de-DE.txt', File.dirname(__FILE__))
+      def self.file_location(lang = 'de-DE')
+        File.expand_path("../../data_files/taxonomy.#{lang}.txt", File.dirname(__FILE__))
       end
 
-      def self.valid?(val)
-        entries.include? val
+      def self.valid?(val, lang = 'de-DE')
+        entries(lang).include? val
       end
 
     end
