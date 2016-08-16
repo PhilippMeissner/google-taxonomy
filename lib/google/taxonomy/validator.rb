18eb10c8b2697b0a5f1d5d89b853feb4ae1bfa23
philippmeissner <pm@zeit.io>
2016-08-16 18:32:39 +0200

    added method to retrieve entries as hash and retrieve hash level-wise

--- lib/google/taxonomy/validator.rb
+++ lib/google/taxonomy/validator.rb
@@ -5,8 +5,8 @@ module Google
     class Validator
 
       def self.entries(lang = 'de-DE')
-        @@entries       ||= {}
-        @@entries[lang] ||= convert_entries(lang)
+        @@entries         ||= {}
+        @@entries[lang]   ||= convert_entries(lang)
       end
 
       def self.convert_entries(lang = 'de-DE')
@@ -26,6 +26,25 @@ module Google
         entries(lang).include? val
       end
 
+      def self.hashed_entries(lang = 'de-DE')
+        split_entries = entries(lang).map {|e| e.split(/ > /)}
+        return split_entries.reduce({}) do |hash, entry|
+          hash.deep_merge(entry.reverse.inject({}) {|k, v| {v => k}})
+        end
+      end
+
+      def self.get_next_level(categories = [], hash = hashed_entries())
+        if categories.blank?
+          return hash
+        else
+          next_level = hash["#{categories[0]}"]
+          if next_level.blank?
+            return hash
+          else
+            get_next_level(categories.drop(1), next_level)
+          end
+        end
+      end
     end
   end
 end
